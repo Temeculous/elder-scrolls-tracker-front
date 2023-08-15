c1e18995c900ac03b2ee3c736127ceaf14c57875
temeculous <zacerious@gmail.com>
2023-08-15 17:36:06 -0400

    baseURL update

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'http://localhost:8080/api/ESGames',
+  baseURL: 'https://elder-scrolls-tracker-back-a2b1e22157dd.herokuapp.com/',
   headers: {
     'Content-Type': 'application/json'
   }
